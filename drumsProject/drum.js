5eda8929d78aedaa0872b93a11258884c99ea53c
venkatesh raparthy <raparthyvenky8367@gmail.com>
2022-12-12 11:26:26 +0530

    first commit

---
+++ drumsProject/drum.js
@@ -0,0 +1,41 @@
+let f1el = document.getElementById('f1');
+let music1=document.getElementById('m1')
+f1el.addEventListener('click',()=>{
+    music1.play();
+})
+
+let f2el = document.getElementById('f2');
+let music2=document.getElementById('m2');
+f2el.addEventListener('click',()=>{
+    music2.play();
+})
+
+let f3el = document.getElementById('f3');
+let music3 =document.getElementById('m3');
+f3el.addEventListener('click',()=>{
+    music3.play();
+})
+
+let f4el = document.getElementById('f4');
+let music4=document.getElementById('m4');
+f4el.addEventListener('click',()=>{
+    music4.play();
+})
+
+let f5el = document.getElementById('f5');
+let music5=document.getElementById('m5');
+f5el.addEventListener('click',()=>{
+    music5.play();
+})
+
+let f6el = document.getElementById('f6');
+let music6=document.getElementById('m6');
+f6el.addEventListener('click',()=>{
+    music6.play();
+})
+
+let f7el = document.getElementById('f7');
+let music7=document.getElementById('m7');
+f7el.addEventListener('click',()=>{
+    music7.play();
+})
